7781b458251394fe746146c5283ccfeb2ace4703
Shoham Jacobsen <shoham.jacobsen@gmail.com>
2020-03-26 16:08:46 +0200

    Added logging for the event handlers using a decorator

---
+++ server/websocket_server/decorators.py
@@ -0,0 +1,36 @@
+import json
+import logging
+from functools import wraps
+from json import JSONDecodeError
+
+one_night_logger = logging.getLogger("one_night")
+one_night_logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+one_night_logger.addHandler(handler)
+
+
+def json_data(event_handler):
+    @wraps(event_handler)
+    async def decorated_event_handler(sid, data):
+        try:
+            data_as_json = json.loads(data)
+            await event_handler(sid, data_as_json)
+        except JSONDecodeError as ex:
+            raise AttributeError("Data must be in JSON format") from ex
+
+    return decorated_event_handler
+
+
+def logger(event_handler):
+    @wraps(event_handler)
+    async def decorated_event_handler(sid, data):
+        event_name = event_handler.__name__
+        one_night_logger.info(f"Event '{event_name}' was called by the client '{sid}' with the data {data}")
+        try:
+            await event_handler(sid, data)
+        except Exception as ex:
+            one_night_logger.error(f"Event '{event_name}' raised '{ex}'")
+
+    return decorated_event_handler
--- server/websocket_server/socketio_server.py
+++ server/websocket_server/socketio_server.py
@@ -4,7 +4,7 @@ import socketio
 from aiohttp import web
 
 from server.rooms.rooms_manager import RoomsManager
-from server.websocket_server.json_decorator import json_data
+from server.websocket_server.decorators import json_data, logger
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
@@ -13,24 +13,27 @@ rooms_manager = RoomsManager(sio)
 
 
 @sio.event
+@logger
 async def connect(sid: str, environ):
-    print("New connection")
     await sio.emit("message", "Welcome to OneNightOnline")
 
 
 @sio.event
+@logger
 @json_data
 async def create_player(sid: str, data: Dict[str, str]):
     rooms_manager.create_player(sid, data["name"])
 
 
 @sio.event
+@logger
 @json_data
 async def add_room(sid: str, data: Dict[str, str]):
     rooms_manager.add_room(sid, [])
 
 
 @sio.event
+@logger
 @json_data
 async def join_room(sid: str, data: Dict[str, str]):
     room_id = data["room_id"]
@@ -38,6 +41,7 @@ async def join_room(sid: str, data: Dict[str, str]):
 
 
 @sio.event
+@logger
 @json_data
 async def start_game(sid: str, data: Dict[str, str]):
     room_id = data["room_id"]
@@ -45,6 +49,7 @@ async def start_game(sid: str, data: Dict[str, str]):
 
 
 @sio.event
+@logger
 @json_data
 async def answer(sid: str, data: Dict[str, str]):
     question_id = data["question_id"]
@@ -53,6 +58,7 @@ async def answer(sid: str, data: Dict[str, str]):
 
 
 @sio.event
+@logger
 @json_data
 async def set_name(sid: str, data: dict):
     name = data["name"]
