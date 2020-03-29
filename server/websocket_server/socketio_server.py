a1023c866e7aa2b745d62ff4a1e5dffbf4c6286d
Shoham Jacobsen <shoham.jacobsen@gmail.com>
2020-03-29 17:43:25 +0300

    Moved responses into RoomsManager's methods

--- server/websocket_server/socketio_server.py
+++ server/websocket_server/socketio_server.py
@@ -3,14 +3,18 @@ from typing import Dict
 import socketio
 from aiohttp import web
 
+from server.cards.alien import Alien
 from server.rooms.rooms_manager import RoomsManager
-from server.websocket_server.decorators import json_data, logger, emit_errors
+from server.websocket_server.decorators import logger, emit_errors
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 rooms_manager = RoomsManager(sio)
 
+STR_TO_CARD = {
+    "alien": Alien
+}
 
 @sio.event
 @logger
@@ -21,55 +25,39 @@ async def connect(sid: str, environ):
 @sio.event
 @logger
 @emit_errors(sio)
-@json_data
 async def create_player(sid: str, data: Dict[str, str]):
-    rooms_manager.create_player(sid, data["name"])
+    await rooms_manager.create_player(sid, data["name"])
 
 
 @sio.event
 @logger
 @emit_errors(sio)
-@json_data
 async def add_room(sid: str, data: Dict[str, str]):
-    game_room_id = rooms_manager.add_room(sid, [])
-    await sio.emit("room_created", {"room_id": game_room_id}, room_id=sid)
+    await rooms_manager.add_room(sid, [STR_TO_CARD[card]() for card in data["cards"]])
 
 
 @sio.event
 @logger
 @emit_errors(sio)
-@json_data
 async def join_room(sid: str, data: Dict[str, str]):
     room_id = data["room_id"]
-    print(f"Joining room '{room_id}'")
+    await rooms_manager.join_room(sid, room_id)
 
 
 @sio.event
 @logger
 @emit_errors(sio)
-@json_data
 async def start_game(sid: str, data: Dict[str, str]):
-    room_id = data["room_id"]
-    print(f"Starting game for room '{room_id}'")
+    await rooms_manager.start_game(sid)
 
 
 @sio.event
 @logger
 @emit_errors(sio)
-@json_data
 async def answer(sid: str, data: Dict[str, str]):
     question_id = data["question_id"]
     user_answer = data["user_answer"]
     print(f"Answer '{user_answer}' for question ID'{question_id}'")
-
-
-@sio.event
-@logger
-@emit_errors(sio)
-@json_data
-async def set_name(sid: str, data: dict):
-    name = data["name"]
-    print(f"Setting name '{name}'")
 
 
 def main():
