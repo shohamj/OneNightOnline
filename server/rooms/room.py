3798cca7183a09336c967dff486543058611b904
Shoham Jacobsen <shoham.jacobsen@gmail.com>
2020-03-29 17:45:00 +0300

    Added an owner and ActionsManager to Room

--- server/rooms/room.py
+++ server/rooms/room.py
@@ -1,16 +1,35 @@
 from typing import List
 
+from server.actions.actions_manager import ActionManager
 from server.cards.card import Card
+from server.games.one_night_alien import OneNightAlien
 from server.players.player import Player
 
 
 class Room:
-    def __init__(self, cards: List[Card]):
-        self.cards = cards
-        self.players = []
+    def __init__(self, owner: Player, cards: List[Card]):
+        self._cards = cards
+        self._owner = owner
+        self._players = []
+        self._action_manager = None
 
     def join(self, player: Player) -> None:
-        self.players.append(player)
+        self._players.append(player)
 
     def is_member(self, player: Player) -> bool:
-        return player in self.players
+        return player in self._players
+
+    def set_action_manager(self, action_manager: ActionManager):
+        self._action_manager = action_manager
+
+    @property
+    def owner(self) -> Player:
+        return self._owner
+
+    @property
+    def players(self) -> List[Player]:
+        return self._players
+
+    async def start_game(self):
+        game = OneNightAlien(self._players, self._cards, self._action_manager)
+        await game.run()
--- server/rooms/rooms_manager.py
+++ server/rooms/rooms_manager.py
@@ -1,14 +1,18 @@
 from random import randint
 from typing import List
-from engineio import AsyncServer
+from socketio import AsyncServer
 
+from server.actions.scoketio_actions_manager import SocketIOActionManager
 from server.cards.card import Card
 from server.exceptions.one_night_exception import OneNightException
 from server.players.player import Player
 from server.rooms.room import Room
 
-PLAYER_NOT_CREATED_ERROR = "A player must be created before adding or joining rooms"
-PLAYER_ALREADY_IN_ROOM_ERROR = "Player has already joined a room"
+ERROR_NOT_ROOM_OWNER = "Only the room's owner can start the game"
+ERROR_ROOM_NOT_FOUND_ERROR = "Room ID not found"
+ERROR_PLAYER_NOT_CREATED_ERROR = "A player must be created before adding or joining rooms"
+ERROR_PLAYER_ALREADY_IN_ROOM = "Player has already joined a room"
+ERROR_PLAYER_NOT_IN_ROOM = "Player is not in a room"
 
 
 def generate_room_id(length: int = 6) -> str:
@@ -22,41 +26,63 @@ class RoomsManager:
         self.sid_to_player = {}
         self.player_to_sid = {}
 
-    def is_player_in_room(self, sid) -> bool:
+    def get_player_room(self, sid):
         player = self.sid_to_player[sid]
-        return any([room.is_member(player) for room in self.rooms.values()])
+        for room in self.rooms.values():
+            if room.is_member(player):
+                return room
 
-    def create_player(self, sid: str, name: str) -> None:
+    async def create_player(self, sid: str, name: str) -> None:
         if sid in self.sid_to_player:
             raise OneNightException("Player already exists")
-
         player = Player(name)
         self.sid_to_player[sid] = player
         self.player_to_sid[player] = sid
+        await self.server.emit("player_created", {"name": name}, room=sid)
 
-    def add_room(self, sid: str, cards: List[Card]) -> str:
+    async def add_room(self, sid: str, cards: List[Card]):
         if sid not in self.sid_to_player:
-            raise OneNightException(PLAYER_NOT_CREATED_ERROR)
-        if self.is_player_in_room(sid):
-            raise OneNightException(PLAYER_ALREADY_IN_ROOM_ERROR)
-        room = Room(cards)
+            raise OneNightException(ERROR_PLAYER_NOT_CREATED_ERROR)
+        if self.get_player_room(sid):
+            raise OneNightException(ERROR_PLAYER_ALREADY_IN_ROOM)
+        player = self.sid_to_player[sid]
+        room = Room(player, cards)
         room_id = generate_room_id()
         self.rooms[room_id] = room
-        room.join(self.sid_to_player[sid])
-        return room_id
+        room.join(player)
+        await self.server.emit("room_created", {"room_id": room_id}, room=sid)
 
-    def join_room(self, sid, room_id) -> None:
+    async def join_room(self, sid, room_id) -> None:
         if sid not in self.sid_to_player:
-            raise OneNightException(PLAYER_NOT_CREATED_ERROR)
-        if self.sid_to_player[sid].room:
-            raise OneNightException(PLAYER_ALREADY_IN_ROOM_ERROR)
-        self.rooms[room_id].join(self.sid_to_player[sid])
+            raise OneNightException(ERROR_PLAYER_NOT_CREATED_ERROR)
+        if self.get_player_room(sid):
+            raise OneNightException(ERROR_PLAYER_ALREADY_IN_ROOM)
+        if room_id not in self.rooms:
+            raise OneNightException(ERROR_ROOM_NOT_FOUND_ERROR)
+        room = self.rooms[room_id]
+        new_player = self.sid_to_player[sid]
+        room.join(new_player)
+        for player in room.players:
+            player_sid = self.player_to_sid[player]
+            print(player_sid)
+            await self.server.emit("player_joined", {"name": new_player.name}, room=player_sid)
 
-    def exit_room(self, sid, room_id):
+    async def exit_room(self, sid, room_id):
         pass
 
-    def start_game(self, sid, room_id):
-        pass
+    async def start_game(self, sid):
+        if sid not in self.sid_to_player:
+            raise OneNightException(ERROR_PLAYER_NOT_CREATED_ERROR)
+        room = self.get_player_room(sid)
+        if not room:
+            raise OneNightException(ERROR_PLAYER_NOT_IN_ROOM)
+        if room.owner != self.sid_to_player[sid]:
+            raise OneNightException(ERROR_NOT_ROOM_OWNER)
+        for player in room.players:
+            player_sid = self.player_to_sid[player]
+            await self.server.emit("game_started", {}, room=player_sid)
+        room.set_action_manager(SocketIOActionManager(self.server, self.sid_to_player, self.player_to_sid))
+        await room.start_game()
 
 
 if __name__ == '__main__':
